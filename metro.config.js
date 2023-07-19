fe93e1c5909474595c17cabeb166760a5b95f779
Corey Phillips <corey.lyle.phillips@proton.me>
2023-07-19 13:22:03 -0400

    chore(wallet): Upgrade RN to 0.72.3
    Upgrades react-native to version 0.72.3.
    Upgrades other dependencies as needed.
    Applies patch to react-native-camera-kit.

--- metro.config.js
+++ metro.config.js
@@ -1,40 +1,42 @@
-/**
- * Metro configuration for React Native
- * https://github.com/facebook/react-native
- *
- * @format
- */
-const { getDefaultConfig } = require('metro-config');
+const path = require('path');
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
 
-const path = require('path');
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
+const customConfig = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false,
+      },
+    }),
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  },
+  resolver: {
+    extraNodeModules: {
+      "sodium-native": path.resolve(__dirname, './node_modules/react-native-libsodium'),
+    },
+    blacklistRE: exclusionList([
+      /android\/build\/nodejs-native-assets-temp-build\/.*/,
+      /\/nodejs-assets\/.*/,
+      /\/node_modules\/sodium-native\/.*/,
+      /\/android\/build\/*/,
+    ])
+  },
+};
 
 module.exports = (async () => {
   const {
     resolver: { sourceExts, assetExts },
   } = await getDefaultConfig();
-  return {
-    transformer: {
-      getTransformOptions: async () => ({
-        transform: {
-          experimentalImportSupport: false,
-          inlineRequires: false,
-        },
-      }),
-      babelTransformerPath: require.resolve('react-native-svg-transformer'),
-    },
-    resolver: {
-      assetExts: assetExts.filter((ext) => ext !== 'svg'),
-      sourceExts: [...sourceExts, 'svg'],
-      extraNodeModules: {
-        "sodium-native": path.resolve(__dirname, './node_modules/react-native-libsodium'),
-      },
-      blacklistRE: exclusionList([
-        /android\/build\/nodejs-native-assets-temp-build\/.*/,
-        /\/nodejs-assets\/.*/,
-        /\/node_modules\/sodium-native\/.*/,
-        /\/android\/build\/*/,
-      ])
-    },
-  };
+  customConfig.resolver.assetExts = assetExts.filter((ext) => ext !== 'svg');
+  customConfig.resolver.sourceExts = [...sourceExts, 'svg'];
+  return mergeConfig(getDefaultConfig(__dirname), customConfig);
 })();
